4d50e2578eb2c4e7b5d4c6e649b33fd2e7f510c5
supermaniszhou <741963634@qq.com>
2021-01-26 11:40:17 +0800

    add-> batch update pwd

--- 1.批量刷密码/src_log/com/seeyon/apps/ext/logRecord/controller/LogRecordController.java
+++ 1.批量刷密码/src_log/com/seeyon/apps/ext/logRecord/controller/LogRecordController.java
@@ -3,9 +3,12 @@ package com.seeyon.apps.ext.logRecord.controller;
 import com.seeyon.ctp.common.controller.BaseController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class LogRecordController extends BaseController {
 
-    public ModelAndView toLogRecordPage() {
+    public ModelAndView toLogRecordPage(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView("apps/ext/logRecord/index");
         return mav;
     }
